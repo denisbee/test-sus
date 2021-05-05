f7bf4d8f24f6b06545d2b24a445746ac391e2d38
Denis Bezrodnykh <denis.bee@gmail.com>
2021-05-05 18:10:33 +0300

    Internal network; fix sql; run tests as testadm

--- roles/app/files/app/testapp.py
+++ roles/app/files/app/testapp.py
@@ -1,4 +1,4 @@
-import psycopg2, json
+import psycopg2, json, os
 from psycopg2.extras import RealDictCursor
 from flask import Flask, request
 
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    with psycopg2.connect(dbname='titlesdb', user='testuser',  password='testpasswd1', host='192.168.88.82') as conn:
+    with psycopg2.connect(dbname='titlesdb',
+        user=os.environ.get('DB_USER', "postgres"),
+        password=os.environ.get('DB_PASSWORD', ""), 
+        host=os.environ.get('DB_HOST', "127.0.0.1")) as conn:
         with conn.cursor(cursor_factory = RealDictCursor) as cursor:
             genre = request.args.get('genre')
             year = request.args.get('year')
             if not genre or not year:
                 return '', 204
             year = int(year)
-            cursor.execute("SELECT * FROM titles WHERE genres LIKE %s AND startYear = %s limit 100", (f'%{genre}%', year))
+            cursor.execute("SELECT * FROM titles WHERE genres LIKE %s AND startYear = %s ORDER BY tconst LIMIT 100", (f'%{genre}%', year))
             return json.dumps([row for row in cursor])
